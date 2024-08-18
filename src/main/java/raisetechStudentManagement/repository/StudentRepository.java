fc65498a488e441b8d39e94410a42bfdee4964ae
HunnyTomato <l2206697704as@gmail.com>
2024-08-18 15:02:57 +0900

    @Updateを二つ追加

--- src/main/java/raisetechStudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetechStudentManagement/repository/StudentRepository.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import raisetechstudentmanagement.data.Student;
 import raisetechstudentmanagement.data.StudentsCourses;
 
@@ -15,12 +16,28 @@ public interface StudentRepository {
   List<Student> search();
 
   @Select("SELECT * FROM students_courses")
-  List<StudentsCourses> searchStudentsCourses();
+  List<StudentsCourses> searchStudentsCoursesList();
 
-  @Select("SELECT * FROM students WHERE age = #{age}")
-  List<Student> searchByAge(int age);
+  @Select("SELECT * FROM students where id = #{id}")
+  Student searchStudent(String id);
+
+  @Select("SELECT * FROM students_courses where studentId = #{studentId}")
+  List<StudentsCourses> searchStudentsCourses(String studentId);
 
   @Insert("insert into students(name, hurigana, nickname, mailAddress, area, age, gender, remark,isDeleted) values(#{name},#{hurigana},#{nickname},#{mailAddress},#{area},#{age},#{gender},#{remark},false)")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudent(Student student);
+
+  @Insert("insert into students_courses(studentId, course, coursesStartDate, coursesExpectedCompletionDate)"
+      + "values(#{studentId}, #{course}, #{coursesStartDate}, #{coursesExpectedCompletionDate})")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void registerStudentsCourses(StudentsCourses studentsCourses);
+
+  @Update("update students set name = #{name}, hurigana = #{hurigana}, nickname = #{nickname},"
+      + " mailAddress = #{mailAddress}, area = #{area}, age = #{age}, gender = #{gender},"
+      + " remark = #{remark}, isDeleted = #{isDeleted} where id = #{id}")
+  void updateStudent(Student student);
+
+  @Update("update students_courses set course = #{course} where studentId = #{studentId}")
+  void updateStudentsCourses(StudentsCourses studentsCourses);
 }
