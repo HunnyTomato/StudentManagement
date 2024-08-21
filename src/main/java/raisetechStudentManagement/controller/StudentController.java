3d92cf59eb51c9fa39459ee313ddda4b57a79e0a
HunnyTomato <l2206697704as@gmail.com>
2024-08-21 16:58:36 +0900

    studentListのModel modelを消去。returnのをconverterに変更。updateStudentはResponseEntityを使った更新処理ができるように変更。

--- src/main/java/raisetechStudentManagement/controller/StudentController.java
+++ src/main/java/raisetechStudentManagement/controller/StudentController.java
@@ -3,6 +3,7 @@ package raisetechstudentmanagement.controller;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,13 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import raisetechstudentmanagement.controller.converter.StudentConverter;
 import raisetechstudentmanagement.data.Student;
 import raisetechstudentmanagement.data.StudentsCourses;
 import raisetechstudentmanagement.domain.StudentDetail;
 import raisetechstudentmanagement.service.StudentService;
 
-@Controller
+@RestController
 public class StudentController {
 
   private StudentService service;
@@ -29,11 +32,10 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public String getStudentList(Model model){
+  public List<StudentDetail> getStudentList(){
     List<Student> students = service.searchStudentList();
     List<StudentsCourses> studentsCourses = service.searchStudentsCoursesList();
-    model.addAttribute("studentList", converter.convertStudentsDetails(students, studentsCourses));
-    return "studentList";
+    return converter.convertStudentsDetails(students, studentsCourses);
   }
 
   @GetMapping("/newStudent")
@@ -61,11 +63,8 @@ public class StudentController {
   }
 
   @PostMapping("/updateStudent")
-  public String updateStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result){
-    if (result.hasErrors()) {
-      return  "updateStudent";
-    }
+  public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail){
     service.updateStudent(studentDetail);
-    return "redirect:/studentList";
+    return ResponseEntity.ok("更新処理が成功しました。");
   }
 }
