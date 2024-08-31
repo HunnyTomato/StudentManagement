fd174213abec0e2f1dc64de3c4fca0620407a5a5
HunnyTomato <l2206697704as@gmail.com>
2024-08-31 16:42:40 +0900

    StudentとStudentCourse,StudentDetailに@Schemaを追加。gradleにOpenAPI Generatorを追加。Applicationに@OpenAPIDefinitionを追加。StudentControllerに@Operationを追加。

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -1,5 +1,6 @@
 package raisetech.student.management.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -30,31 +31,30 @@ public class StudentController {
     this.service = service;
   }
 
+  @Operation(summary = "一覧検索", description = "受講生の一覧を検索します。")
   @GetMapping("/studentList")
   public List<StudentDetail> getStudentList(){
     return service.searchStudentList();
-  }
-
-  @GetMapping("/studentsList")
-  public List<StudentDetail> getStudentsList() throws TestException{
-    throw new TestException("エラーが発生しました。");
   }
 
   @GetMapping("/student/{id}")
   public StudentDetail getStudent(@PathVariable @NotBlank @Pattern(regexp = "^\\d+$") String id){
     return service.searchStudent(id);
   }
+
+  @Operation(summary = "受講生登録", description = "受講生を登録しました。")
   @PostMapping("/registerStudent")
   public ResponseEntity<StudentDetail> registerStudent(@RequestBody @Valid StudentDetail studentDetail){
     StudentDetail responseStudentDetail = service.registerStudent(studentDetail);
     return ResponseEntity.ok(responseStudentDetail);
   }
-
+  @Operation(summary = "受講生更新", description = "更新処理が成功しました。")
   @PutMapping("/updateStudent")
   public ResponseEntity<String> updateStudent(@RequestBody @Valid StudentDetail studentDetail){
     service.updateStudent(studentDetail);
     return ResponseEntity.ok("更新処理が成功しました。");
   }
+
   @ExceptionHandler(TestException.class)
   public ResponseEntity<String> handleTestException(TestException ex) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
--- src/main/java/raisetech/student/management/data/StudentCourse.java
+++ src/main/java/raisetech/student/management/data/StudentCourse.java
@@ -1,9 +1,10 @@
 package raisetech.student.management.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
-
+@Schema(description = "受講生コース情報")
 @Getter
 @Setter
 
